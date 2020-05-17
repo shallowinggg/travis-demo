0e91634df9c6b36dc52683b0b0dc982dc2c4a8ec
shallowinggg <shallowinggg@icloud.com>
2020-05-17 16:26:38 +0800

    Initial commit

---
+++ src/test/java/com/example/travisdemo/TravisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
